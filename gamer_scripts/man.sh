ba6cf1190490506d3ef2034582adb92287a53267
Your Name <you@example.com>
2021-09-12 16:53:06 +0530

    black arch in

--- gamer_scripts/man.sh
+++ gamer_scripts/man.sh
@@ -13,7 +13,6 @@ sudo pacman -Syyu
 sudo pacman -Syy dhcpcd bluez bluez-utils cups xf86-video-intel xf86-video-amdgpu nvidia fish nvidia-utils virtualbox-host-dkms virtualbox-host-modules-arch firefox brave nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
 sudo systemctl enable bluetooth
 sudo echo "/usr/local/bin/fish" >> /etc/shells
-
 chsh -s /usr/local/bin/fish
 sudo systemctl enable org.cups.cupsd
 rm -r a
