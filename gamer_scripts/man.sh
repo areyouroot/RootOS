d415aa492ac8ada66507229209190d03b2f743e5
Your Name <you@example.com>
2021-09-12 15:34:16 +0530

    fish added

--- gamer_scripts/man.sh
+++ gamer_scripts/man.sh
@@ -8,8 +8,10 @@ read ans
 sudo echo -e "nameserver 1.1.1.1\nnameserver 1.0.0.1" > /etc/resolv.conf
 sudo pacman -Fyy
 sudo pacman -Syyu
-sudo pacman -Syy dhcpcd bluez bluez-utils cups xf86-video-intel xf86-video-amdgpu nvidia nvidia-utils virtualbox-host-dkms virtualbox-host-modules-arch firefox brave nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
+sudo pacman -Syy dhcpcd bluez bluez-utils cups xf86-video-intel xf86-video-amdgpu nvidia fish nvidia-utils virtualbox-host-dkms virtualbox-host-modules-arch firefox brave nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
 sudo systemctl enable bluetooth
+echo "/usr/local/bin/fish" >> /etc/shells
+chsh -s /usr/local/bin/fish
 sudo systemctl enable org.cups.cupsd
 
 #man enabling games
