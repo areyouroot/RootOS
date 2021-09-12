b566d3ea001ec23c62f4ee3b41a05f18960c1aef
Your Name <you@example.com>
2021-09-12 07:41:38 +0530

    manually running install.sh

--- install_arch.sh
+++ install_arch.sh
@@ -31,7 +31,27 @@ pacman -Fyy
 pacman -S reflector
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
-pacstrap /mnt base linux linux-firmware
+pacstrap /mnt base linux linux-firmware xorg sddm plasma kde-applications sudo nano
+genfstab -U /mnt >> /mnt/etc/fstab
+echo "enter the new user name : "
+read new_user
+echo $new_user > /mnt/etc/hostname
+touch /mnt/etc/hosts
+echo -e "127.0.0.1 localhost \n::1 localhost\n127.0.1.1 user" >> /mnt/etc/hosts
+echo "enter the root password"
+arch-chroot /mnt passwd
+arch-chroot /mnt pacman -Syy grub os-prober
+arch-chroot /mnt grub-install $disk
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+arch-chroot /mnt useradd -m $new_user
+arch-chroot /mnt passwd $new_user
+sed '/root ALL=(ALL) ALL/s/$/ \n'$new_user'ALL=(ALL) ALL\n&/' /mnt/etc/sudoers > /mnt/etc/sudoers
+arch-chroot /mnt systemctl enable sddm
+arch-chroot /mnt systemctl enable NetworkManager
+sed '/Current=/s/$/ \nbreeze\n&/' /mnt/usr/lib/sddm/sddm.conf.d/default.conf > /mnt/usr/lib/sddm/sddm.conf.d/default.conf
+echo "your os has been installed u can reboot now and boot into the existing os"
+
 
 
 
