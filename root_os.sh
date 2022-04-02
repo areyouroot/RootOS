3a81ce9b6c5488dd8611d8c987643296793ab193
Your Name <you@example.com>
2022-04-02 19:05:19 +0530

    gui install beta 1

--- root_os.sh
+++ root_os.sh
@@ -5,7 +5,16 @@ sudo pacman -Syy
 sudo pacman -Fyy
 sudo pacman -S sudo nano
 echo -e "now the visudo will be opened using nano u just need \n to need to add ur user name with the permisions eg add a \n line below like if ur user name is sam \nroot ALL=(ALL:ALL) ALL\nsam ALL=(ALL) ALL\n remember every thing is case sensitive so put all in caps\ntype ok to continue"
+read verify
 EDITOR=nano visudo
 pacman -S xorg plasma plasma-wayland-session kde-applications 
 systemctl enable sddm.service
 systemctl enable NetworkManager.service
+pacman -Syu
+uname -r
+sudo pacman -S linux-lts linux-lts-headers yaourt yay
+sudo pacman -Rs linux
+yaourt -S pamac-aur
+sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins
+sudo pacman -S vlc libreoffice thunderbird firefox gedit flashplugin skype dropbox aria2 p7zip p7zip-plugins unrar tar rsync conky
+
